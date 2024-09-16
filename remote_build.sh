e437ff1fcb4989009e6c846d6e3b67873ab6732c
Alex <alex.algarate@gmail.com>
2024-09-16 17:27:34 +0200

    2nd commit

--- remote_build.sh
+++ remote_build.sh
@@ -5,6 +5,6 @@ isort wedding/
 black wedding/
 ruff check --fix wedding/ 
 reflex init
-reflex export --frontend-only
+API_URL=https://wedding-backend.up.railway.app reflex export --frontend-only
 unzip frontend.zip -d public
 rm -f frontend.zip
