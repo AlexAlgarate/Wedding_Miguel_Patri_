f1e615a846d8e5d9b07f55973e786db8d7d3567d
Alex <alex.algarate@gmail.com>
2024-09-17 08:55:09 +0200

    Drop echo in remote build.sh

--- remote_build.sh
+++ remote_build.sh
@@ -1,31 +1,10 @@
 python -m pip install --upgrade pip
 pip install -r requirements.txt
-echo "INSTALADO"
-echo "INSTALADO"
-echo "INSTALADO"
-echo "INSTALADO"
 rm -fr public
 isort wedding/
 black wedding/
 ruff check --fix wedding/
-echo "LINTER PASADO"
-echo "LINTER PASADO" 
-echo "LINTER PASADO"
-echo "LINTER PASADO"
 reflex init
-echo "REFLEX INIT"
-echo "REFLEX INIT"
-echo "REFLEX INIT"
-echo "REFLEX INIT"
-echo "REFLEX INIT"
 API_URL=https://weddingmiguelpatri-production.up.railway.app reflex export --frontend-only
-echo "REFLEX EXPORTED"
-echo "REFLEX EXPORTED"
-echo "REFLEX EXPORTED"
-echo "REFLEX EXPORTED"
 unzip frontend.zip -d public
-echo "PUBLIC CREADO"
-echo "PUBLIC CREADO"
-echo "PUBLIC CREADO"
-echo "PUBLIC CREADO"
 rm -f frontend.zip
