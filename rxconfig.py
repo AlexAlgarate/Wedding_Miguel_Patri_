5f094f0960fa8bd9aadcafc84c686751b2c3e590
Alex <alex.algarate@gmail.com>
2024-09-17 17:40:26 +0200

    add CORS

--- rxconfig.py
+++ rxconfig.py
@@ -1,3 +1,9 @@
 import reflex as rx
 
-config = rx.Config(app_name="wedding")
+config = rx.Config(
+    app_name="wedding",
+    cors_allowed_origins=[
+        "http://localhost:3000/",
+        "https://wedding-patri-miguel.vercel.app/",
+    ],
+)
